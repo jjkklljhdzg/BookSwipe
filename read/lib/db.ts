0b982cb3dd1a8af4817be6d052fcc89930b7f22f
kohuru <sidorovasona00@gmail.com>
2026-01-12 14:16:35 +0400

    Delete read directory

--- read/lib/db.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-const Database = require('better-sqlite3');
-
-const dbPath = path.join(process.cwd(), 'BookSwipe.db');
-
-export const db = new Database(dbPath, {
-  readonly: true,
-});
